ca5c87951c52a814146351cc266cd50e9e106294
undefcat <undefcat@gmail.com>
2021-07-04 23:48:04 +0900

    feat(Model): File, Product morphMany relation 설정

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Product extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function files()
+    {
+        return $this->morphMany(File::class, 'fileable');
+    }
 }
