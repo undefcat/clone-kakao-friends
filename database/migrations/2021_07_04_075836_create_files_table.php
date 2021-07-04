c1fba529f173ead3a474d22e9ac91e0887b22537
undefcat <undefcat@gmail.com>
2021-07-04 17:20:23 +0900

    feat(file): File 기본 모델, 마이그레이션 생성

---
+++ database/migrations/2021_07_04_075836_create_files_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('fileable_id')->unsigned();
+            $table->string('fileable_type');
+            $table->bigInteger('size')->unsigned()->comment('파일크기(Octet)');
+            $table->string('tag');
+            $table->string('mime_type', 30);
+            $table->string('mime_subtype');
+            $table->string('original_name')->comment('원본파일명');
+            $table->string('path', 1024)->comment('저장경로');
+            $table->dateTime('created_at')->nullable();
+            $table->dateTime('updated_at')->nullable();
+
+            $table->index('fileable_id');
+            $table->index('fileable_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
